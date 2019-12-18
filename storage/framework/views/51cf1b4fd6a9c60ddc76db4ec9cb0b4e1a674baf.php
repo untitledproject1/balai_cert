6ae6ac298b1c6176d318ae1c63adc82528f30996
ArdhinNaufal <haalozha@gmail.com>
2019-12-18 14:25:05 +0700

    Update .gitignore

--- storage/framework/views/51cf1b4fd6a9c60ddc76db4ec9cb0b4e1a674baf.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH /var/www/html/balai-cert/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
--- storage/framework/views/d253d9c082f2f7dafb433e1f6d3804bcd73d9f39.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-    <div class="col-lg-12">
-        <div class="footer_main">
-            Copyright &copy; 2019 
-        </div>
-    </div>
-</div>
-<?php /**PATH /var/www/html/balai-cert/resources/views/layouts/footer.blade.php ENDPATH**/ ?>
